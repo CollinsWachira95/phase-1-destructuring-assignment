2711f19e38b5708297c0935a01838808a2a3ed7e
Collins <collins.wachira@student.moringaschool.com>
2024-07-21 23:56:45 +0300

    new file

--- index.js
+++ index.js
@@ -1,48 +1,98 @@
-const farmAnimals = 'cow horse sheep pig chicken';
+// index.js
 
-const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
+// Destructuring Strings
+const animalSounds = {
+  moo: 'cow',
+  neigh: 'horse',
+  baa: 'sheep',
+  oink: 'pig',
+  cluck: 'chicken'
+};
 
+const { moo, neigh, baa, oink, cluck } = animalSounds;
+
+const animalNames = {
+  bessie: 'cow',
+  dolly: 'sheep',
+  babe: 'pig',
+  little: 'chicken'
+};
+
+const { bessie, dolly, babe, little } = animalNames;
+
+const animalColors = {
+  blackAndWhite: 'cow',
+  black: 'sheep',
+  pink: 'pig'
+};
+
+const { blackAndWhite, black, pink } = animalColors;
+
+// Destructuring Arrays
+const rainbowColors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
+
+const [red, orange, yellow, green, blue, indigo, violet] = rainbowColors;
+
+const [r, o, y, g, b, , v] = rainbowColors;
+
+const [, , , , , indg] = rainbowColors;
+
+// Destructuring Objects
 const muppet = {
   muppetName: 'Miss Piggy',
   color: 'pink',
   song: 'Never Before, Never Again',
   job: 'Cast member of The Muppet Show',
-  partner: 'Kermit'
+  partner: 'Kermit',
+  songs: [
+    'The Rainbow Connection',
+    'Moving Right Along',
+    'Bein\' Green',
+    'I Hope That Something Better Comes Along'
+  ],
+  nested: {
+    job: 'Host of The Muppet Show',
+    partner: 'Miss Piggy'
+  }
 };
 
-const nestedMuppet = {
-  nestedName: 'Kermit',
-  nestedColor: 'green',
-  album: {
-    theMuppetMovie: {
-      song1: 'Rainbow Connection',
-      song2: 'Moving Right Along',
-      song3: 'Never Before, Never Again',
-      song4: 'I Hope That Something Better Comes Along',
-    },
-  },
-  nestedJob: 'Host of The Muppet Show',
-  nestedPartner: 'Miss Piggy'
+const { muppetName, color, song, job, partner } = muppet;
+const { songs: [, song2, , song4], nested: { job: nestedJob, partner: nestedPartner } } = muppet;
+
+module.exports = {
+  moo,
+  neigh,
+  baa,
+  oink,
+  cluck,
+  bessie,
+  dolly,
+  babe,
+  little,
+  blackAndWhite,
+  black,
+  pink,
+  red,
+  orange,
+  yellow,
+  green,
+  blue,
+  indigo,
+  violet,
+  r,
+  o,
+  y,
+  g,
+  b,
+  v,
+  indg,
+  muppetName,
+  color,
+  song,
+  job,
+  partner,
+  song2,
+  song4,
+  nestedJob,
+  nestedPartner
 };
-
-// Strings
-
-// 1. Use destructuring to assign appropriate variables based on the sounds animals make.
-
-// 2. Bolt the horse wandered off, so just give us four animals, and let's name them bessie, dolly, babe, and little.
-
-// 3. Little the chicken had to go back to the coop, so now we're left with three. Let's use color variables of blackAndWhite, black, and pink.
-
-// Arrays
-
-// 4. Use destructuring to assign appropriate variables using the color names.
-
-// 5. Some people have a really hard time picking out indigo, so let's leave that one out, using the first letter of each color as the variable names.
-
-// 6. But wait! Indigo is now feeling *super* left out. Let's only assign indigo using indg. 
-
-// Objects
-
-// 7. Use destructuring to assign all variables using the keys as the variable names
-
-// 8. Use destructuring to assign songs 2 and 4, and Kermit's job and partner
